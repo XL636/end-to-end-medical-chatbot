1c0a3ce3864cf3a4602d3e4d1122fee2816e2197
XL636 <l.xinghong@wustl.edu>
2025-03-05 22:21:35 -0600

    upload setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Lex li',
+    author_email= 'lxh04112002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
